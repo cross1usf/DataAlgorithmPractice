cbf22c3abfd528f5f856ed9396f07a921ac9ef15
cross1usf <jakeedcross@gmail.com>
2022-05-28 18:27:14 -0400

    feat - IdenticalPairs Algorithm

---
+++ src/com/company/Arrays/IdenticalPairs.java
@@ -0,0 +1,16 @@
+package com.company.Arrays;
+
+public class IdenticalPairs {
+
+    public int numIdenticalPairs(int[] nums) {
+        int pairs = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    pairs++;
+                }
+            }
+        } return pairs;
+    }
+
+}
