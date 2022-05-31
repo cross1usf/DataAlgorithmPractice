a8414cc2903cad06e93e6f22330ce06c044183a7
cross1usf <jakeedcross@gmail.com>
2022-05-30 20:48:52 -0400

    feat - binary # of one bits

---
+++ src/com/company/binary/NumberOfOneBits.java
@@ -0,0 +1,12 @@
+package com.company.binary;
+
+public class NumberOfOneBits {
+    public int hammingWeight(int n) {
+    int count = 0, mask =1;
+        for (int i = 0; i < 32; i++) {
+            if((n & mask) != 0) count++;
+            mask = mask << 1;
+        }
+        return count;
+    }
+}
