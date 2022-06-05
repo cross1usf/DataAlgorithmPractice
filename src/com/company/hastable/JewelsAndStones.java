4b60ab564eb5900d849d4d641fc5c86a41844a8a
cross1usf <jakeedcross@gmail.com>
2022-06-05 15:17:17 -0400

    feat - Jewewls & Stones Algorithm

---
+++ src/com/company/hastable/JewelsAndStones.java
@@ -0,0 +1,23 @@
+package com.company.hastable;
+
+import java.util.HashSet;
+
+public class JewelsAndStones {
+
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        HashSet set = new HashSet();
+        for (int i = 0; i < jewels.length(); i++) {
+            set.add(jewels.charAt(i));
+        }
+        for (int i = 0; i < stones.length(); i++) {
+           if(set.contains(stones.charAt(i)))
+               count++;
+        }
+
+        return count;
+        }
+
+    }
+
+
